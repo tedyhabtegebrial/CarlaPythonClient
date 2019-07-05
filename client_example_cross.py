ec8f236d05531be1f791c7edc72c2046e2e23e0c
Tewodros Amberbir <tedyhabtegebrial@gmail.com>
2019-07-05 11:44:49 +0200

    Town2 got stuck: re-running

--- client_example_cross.py
+++ client_example_cross.py
@@ -33,7 +33,7 @@ def run_carla_client(args):
     frames_per_episode = 10030
     #              [0  , 1  , 2  , 3  , 4  , 5  , 6 , 7, 8  , 9  , 10, 11, 12, 13, 14]
     # vehicles_num = [60, 60, 70, 50, 60, 60, 80, 60, 60, 60, 50, 70, 60, 50, 50]
-    vehicles_num = [35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 40, 40, 40]
+    vehicles_num = [35, 35, 30, 35, 35, 35, 35, 35, 35, 35, 35, 35, 40, 40, 40]
     # vehicles_num = [60, 60, 70, 50, 60, 60, 80, 60, 60, 60, 50, 70, 60, 50, 50]
 
     # We assume the CARLA server is already waiting for a client to connect at
@@ -43,7 +43,8 @@ def run_carla_client(args):
     # context manager makes sure the connection is always cleaned up on exit.
     with make_carla_client(args.host, args.port) as client:
         print('CarlaClient connected')
-        list_of_episodes = [0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 14]
+        # list_of_episodes = [0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 14]
+        list_of_episodes = [2, 3, 4, 5, 6, 7, 9, 11, 14]
         #list(range(0, number_of_episodes))
         #list_of_episodes.pop(13)
         #list_of_episodes.pop(12)
